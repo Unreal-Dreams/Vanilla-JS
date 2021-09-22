0ba76436293ace4e830900bcce09a6533e049ea9
Vaibhav Kumar <vaibhavvats1000@gmail.com>
2021-09-23 05:16:00 +0530

    Learning Web D

---
+++ main.js
@@ -0,0 +1,13 @@
+const result={
+    physics:{
+        midTerm:20,
+        endterm:60
+    },
+    chemistry:{
+        midTerm:25,
+        endTerm:[20,30,60]
+    }
+}
+console.log(result.chemistry.endTerm[0]);
+result.email="vaibhavats1000@gmail.com";
+console.log(result);
